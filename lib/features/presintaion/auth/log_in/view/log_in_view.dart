25b2ed4398d5e1fc665f40fa3eaca5b88176f335
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-11-30 03:03:41 +0200

    create logIn view

---
+++ lib/features/presintaion/auth/log_in/view/log_in_view.dart
@@ -0,0 +1,13 @@
+import 'package:chit_chat/features/presintaion/auth/log_in/view/widgets/sign_up_body.dart';
+import 'package:chit_chat/features/presintaion/auth/sign_up/view/widgets/sign_up_body.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: SingleChildScrollView(child: SafeArea(child: LoginBody())));
+  }
+}

fdc24666b237001d66416f8537d53a213e867ae5
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-12-01 01:44:11 +0200

    create custom app bar

---
+++ lib/features/home/presentation/view/widgets/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:chit_chat/constans.dart';
+import 'package:chit_chat/core/utils/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+    AppBar(
+      backgroundColor: kPrimaryColor,
+      leading: IconButton(onPressed: (){},
+     icon: const Icon( Icons.menu),color: Colors.white,),
+     title: Center(child: Text('Groups',style: Style.textStyleBold24.copyWith(color: Colors.white),)),
+    actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.search),color: Colors.white,)],
+     )
+    ;
+  }
+}

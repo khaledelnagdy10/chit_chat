ac9929ceda8300cd52cfc82e64104f94161a35d0
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-11-30 03:05:40 +0200

    add pic to pubspec.yaml

--- lib/features/presintaion/auth/log_in/view/log_in_view.dart
+++ lib/features/presintaion/auth/log_in/view/log_in_view.dart
@@ -1,4 +1,4 @@
-import 'package:chit_chat/features/presintaion/auth/log_in/view/widgets/sign_up_body.dart';
+import 'package:chit_chat/features/presintaion/auth/log_in/view/widgets/log_in_body.dart';
 import 'package:chit_chat/features/presintaion/auth/sign_up/view/widgets/sign_up_body.dart';
 import 'package:flutter/material.dart';
 

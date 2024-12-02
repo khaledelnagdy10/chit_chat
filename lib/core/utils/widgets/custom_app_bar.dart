092c420d40d87a0961cd8803c4e19403edb614ad
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-12-02 15:06:02 +0200

    refactor custom app_bar

--- lib/core/utils/widgets/custom_app_bar.dart
+++ lib/core/utils/widgets/custom_app_bar.dart
@@ -3,21 +3,19 @@ import 'package:chit_chat/core/utils/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, required this.text,  required this.leadingIcon, required this.actionIcon, required this.leadingIconOnPressed, required this.actionIconOnPressed});
+  const CustomAppBar({super.key, required this.text, required this.actionIcon, });
 final String text;
-final void Function() leadingIconOnPressed;
-final void Function() actionIconOnPressed;
-final Icon leadingIcon;
+
+
 final Icon actionIcon;
   @override
   Widget build(BuildContext context) {
     return 
     AppBar(
       backgroundColor: kPrimaryColor,
-      leading: IconButton(onPressed: leadingIconOnPressed,
-     icon: leadingIcon),
+     
      title: Center(child: Text(text,style: Style.textStyleBold24.copyWith(color: Colors.white),)),
-    actions: [IconButton(onPressed:actionIconOnPressed , icon: actionIcon,)],
+    actions: [IconButton(onPressed:(){} , icon: actionIcon,)],
      )
     ;
   }

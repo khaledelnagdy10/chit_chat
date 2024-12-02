4e72e855185934b9c89e4e275be5f6cbd6689f2a
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-12-02 21:07:21 +0200

    read data from fire store & add it in profile info

--- lib/features/profile/presentation/view/profile_view.dart
+++ lib/features/profile/presentation/view/profile_view.dart
@@ -7,7 +7,7 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       drawer: NavBar(),
       body: ProfileBody(),
     );
--- lib/features/profile/presentation/view/widgets/profile_body.dart
+++ lib/features/profile/presentation/view/widgets/profile_body.dart
@@ -1,40 +1,60 @@
 import 'package:chit_chat/core/utils/widgets/custom_app_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ProfileBody extends StatelessWidget {
-  const ProfileBody({super.key});
-
+   ProfileBody({super.key});
+CollectionReference db = FirebaseFirestore.instance.collection('user');
+ String userId ='KNToYk5gwLUz2DiouAjI';
+ 
   @override
   Widget build(BuildContext context) {
-    return   Column(
-      children: [
-        const CustomAppBar(text: 'Profile',  
-        actionIcon: Icon(Icons.search)),
-        const SizedBox(height: 50,),
-    const SizedBox(height: 90,),
-        Icon(Icons.account_circle,size: 150,color: Colors.grey[750],),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal:  40.0),
-          child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-               Text('Full Name  '),
-               
-               Text('Khaled elnagdy')
-            ],
-          ),
-        ), 
-        const Divider(),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal:  40.0),
-          child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-               Text('Email'),
-               
-               Text('Khaledelnagdy@gmail.com')
-            ],
-          ),
-        ), 
-      ],
+    return   FutureBuilder<DocumentSnapshot>( 
+           future: db.doc(userId).get(),
+
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text('something went wrong');
+        } 
+         if (!snapshot.hasData || !snapshot.data!.exists) {
+          return const Center(child: Text('User not found'));
+        }
+
+       Map<String,dynamic>? userData = snapshot.data !.data() as Map<String,dynamic>;
+       String fullName =  userData ['full name']?? 'N/A';
+       String email =  userData ['email']?? 'N/A'; 
+      return Column(
+        children: [
+          const CustomAppBar(text: 'Profile',  
+          actionIcon: Icon(Icons.search)),
+          const SizedBox(height: 50,),
+      const SizedBox(height: 90,),
+          Icon(Icons.account_circle,size: 150,color: Colors.grey[750],),
+           Padding(
+            padding: const EdgeInsets.symmetric(horizontal:  40.0),
+            child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                 const Text('Full Name  '),
+                 
+                 Text(fullName)
+              ],
+            ),
+          ), 
+          const Divider(),
+           Padding(
+            padding: const EdgeInsets.symmetric(horizontal:  40.0),
+            child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                 const Text('Email'),
+                 
+                 Text(email)]
+            ),
+          ), 
+        ],
+      );
+      
+      },
+      
     )
      ;
   }

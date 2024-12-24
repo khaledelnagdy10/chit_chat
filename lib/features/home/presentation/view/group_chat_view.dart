632d216ef25b43041bce7a644d928ee079f65e86
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-12-24 18:07:01 +0200

    create group chat view & body

---
+++ lib/features/home/presentation/view/group_chat_view.dart
@@ -0,0 +1,11 @@
+import 'package:chit_chat/features/home/presentation/view/widgets/group_chat_body.dart';
+import 'package:flutter/material.dart';
+
+class GroupChatView extends StatelessWidget {
+  const GroupChatView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GroupChatBody();
+  }
+}

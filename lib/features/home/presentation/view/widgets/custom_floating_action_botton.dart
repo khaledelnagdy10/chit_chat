e224143bb01e89fc8aff3aa9d753f1c914a22fe6
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-12-24 18:03:31 +0200

    add bloc provider to custom floating button

--- lib/features/home/presentation/view/widgets/custom_floating_action_botton.dart
+++ lib/features/home/presentation/view/widgets/custom_floating_action_botton.dart
@@ -1,66 +1,64 @@
 import 'package:chit_chat/constans.dart';
+import 'package:chit_chat/features/home/data/manager/home_cubit/home_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomFloatingActionButton extends StatelessWidget {
-  const CustomFloatingActionButton({super.key});
-
+  CustomFloatingActionButton({super.key});
+  final TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: (){
-showDialog(context: context, builder: (context){
-
-  return AlertDialog(
-    title: Text('Create new group'),
-
-    content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Group Name",
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-
-                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: const Text('Create new group'),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                       ElevatedButton( 
-                      onPressed: () {
-                        Navigator.pop(context); 
-                      },
-                      child: Text("Create"),
-                    ),
-                    SizedBox(width: 5,),
-                    ElevatedButton( 
-                      onPressed: () {
-                        Navigator.pop(context); 
-                      },
-                      child: Text('Cancel'),
-                    ),
-                   ],)
-
-
-
-                
-                  ],
-                ),
-
-
-  );
-}
-
-);
-
-    },
-    child: 
-
-    Container(
-      decoration: const BoxDecoration(shape: BoxShape.circle,color: kPrimaryColor) ,
-      child:   const Icon(Icons.add))
-    
-    );
+                      TextField(
+                        controller: controller,
+                        decoration: const InputDecoration(
+                          labelText: "Group Name",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              final groupName = controller.text.trim();
+                              if (groupName.isNotEmpty) {
+                                BlocProvider.of<HomeCubit>(context)
+                                    .homeCubit(groupName);
+                                Navigator.pop(context);
+                              }
+                            },
+                            child: const Text("Create"),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancel'),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              });
+        },
+        child: Container(
+            decoration: const BoxDecoration(
+                shape: BoxShape.circle, color: kPrimaryColor),
+            child: const Icon(Icons.add)));
   }
 }
